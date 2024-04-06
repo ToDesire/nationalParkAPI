72b8126afe2565d72ef113112c07c4aadc86dac0
ToDesire <rtodesire@gmail.com>
2024-04-06 12:41:45 +0300

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Client } from './typeorm/entities/client.entity';
+import { Reservation } from './typeorm/entities/reservation.entity';
+import { Circuit } from './typeorm/entities/circuit.entity';
+import { Guide } from './typeorm/entities/guide.entity';
+import { Landscape } from './typeorm/entities/landscape.entity';
+import { Picture } from './typeorm/entities/picture.entity';
+import { Session } from './typeorm/entities/session.entity';
+import { Specie } from './typeorm/entities/specie.entity';
+import { Tag } from './typeorm/entities/tag.entity';
+import { ClientModule } from './client/client.module';
+import { TagModule } from './tag/tag/tag.module';
+import { Evenement } from './typeorm/entities/evenement.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'national_park',
+    entities: [Circuit, Client, Evenement, Guide, Landscape, Picture, Reservation, Session, Specie, Tag],
+    synchronize: true,
+  }), ClientModule, TagModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
